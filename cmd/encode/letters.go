1c0acaa5d89042352a487cc8b2627574bc9eccea
Jabez Villamarzo <math2btrue@gmail.com>
2025-09-03 12:46:34 +0800

    feat: Generate sfx sounds, adjustable morse code sound speed

--- cmd/encode/letters.go
+++ cmd/encode/letters.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbletea"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/gopxl/beep"
 	"github.com/gopxl/beep/speaker"
 	"github.com/noAbbreviation/dihdah/commons"
@@ -25,40 +25,63 @@ var LetterCmd = &cobra.Command{
 }
 
 type letterModel struct {
-	m    *commons.TrainingModel
-	done <-chan bool
+	m *commons.TrainingModel
 }
 
 func newLetterModel() letterModel {
-	done := make(chan bool)
-	go func() {
-		message := "dooby"
-		delayBuffer := commons.SoundAssets[commons.ShortDelay]
+	return letterModel{}
+}
 
-		firstMorseCode := commons.MorseCodeLookup[rune(message[0])]
-		speaker.PlayAndWait(commons.MorseCharSound(firstMorseCode))
+type doneMsg struct{}
 
-		for _, c := range message[1:] {
-			morseCode := commons.MorseCodeLookup[c]
-			speaker.PlayAndWait(beep.Seq(
-				beep.Loop(3, delayBuffer.Streamer(0, delayBuffer.Len())),
-				commons.MorseCharSound(morseCode),
-			))
+func playMorseCode() tea.Msg {
+	speed := float64(1)
+	delayBuffer := commons.SoundAssets[commons.ShortDelay]
+	delayStreamer := delayBuffer.Streamer(0, delayBuffer.Len())
+	delayResampler := beep.ResampleRatio(4, speed, delayStreamer)
+
+	emptyStreamer := beep.NewBuffer(commons.AudioFormat)
+	emptyStreamer.Append(delayResampler)
+
+	message := "the quick brown fox jumps over the lazy dog"
+
+	firstMorseCode := commons.MorseCodeLookup[rune(message[0])]
+	speaker.PlayAndWait(commons.MorseCharSound(firstMorseCode, speed))
+
+	for _, c := range message[1:] {
+		streamer := emptyStreamer.Streamer(0, emptyStreamer.Len())
+
+		if c == ' ' {
+			speaker.PlayAndWait(beep.Loop(7, streamer))
+			continue
 		}
 
-		done <- true
-	}()
+		morseCode := commons.MorseCodeLookup[c]
 
-	return letterModel{done: done}
+		speaker.PlayAndWait(beep.Loop(3, streamer))
+		speaker.PlayAndWait(commons.MorseCharSound(morseCode, speed))
+	}
+
+	return doneMsg{}
 }
 
 func (_m letterModel) Init() tea.Cmd {
-	return nil
+	return playMorseCode
 }
 
 func (_m letterModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	<-_m.done
-	return _m, tea.Quit
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return _m, tea.Quit
+		}
+
+	case doneMsg:
+		return _m, tea.Quit
+	}
+
+	return _m, nil
 }
 
 func (_m letterModel) View() string {
--- commons/sfx.go
+++ commons/sfx.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/gopxl/beep"
-	"github.com/gopxl/beep/mp3"
+	"github.com/gopxl/beep/generators"
 	"github.com/gopxl/beep/speaker"
 )
 
@@ -18,51 +18,67 @@ const (
 	ShortDelay
 )
 
-var (
-	AudioFormat beep.Format
-	SoundAssets = map[soundType]*beep.Buffer{}
-)
+const DefaultDitDuration = time.Millisecond * 60
+
+var SoundAssets map[soundType]*beep.Buffer
+
+var AudioFormat = beep.Format{
+	SampleRate:  24_000,
+	NumChannels: 2,
+	Precision:   2,
+}
 
 type audioBuffer struct {
 	buffer beep.Buffer
 }
 
 func init() {
-	soundFilePrefixes := map[soundType]string{
-		ShortBeep:  "short-beep",
-		LongBeep:   "long-beep",
-		ShortDelay: "short-delay",
-	}
-
-	for soundType, filePrefix := range soundFilePrefixes {
-		fileName := fmt.Sprintf("./assets/sfx_%v.mp3", filePrefix)
-
-		file, err := os.Open(fileName)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error opening %v: %v", fileName, err)
-			os.Exit(1)
-		}
-
-		var streamer beep.StreamSeekCloser
-		streamer, AudioFormat, err = mp3.Decode(file)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error decoding %v: %v", fileName, err)
-			os.Exit(1)
-		}
-
-		buffer := beep.NewBuffer(AudioFormat)
-		buffer.Append(streamer)
-		streamer.Close()
-
-		SoundAssets[soundType] = buffer
-	}
-
+	initSoundAssets(DefaultDitDuration)
 	speaker.Init(AudioFormat.SampleRate, AudioFormat.SampleRate.N(time.Second/10))
 }
 
-func MorseCharSound(str string) beep.Streamer {
+func initSoundAssets(ditDuration time.Duration) {
+	shortBeepSamples := AudioFormat.SampleRate.N(ditDuration)
+
+	audioTone, err := generators.SawtoothTone(AudioFormat.SampleRate, 1_000)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating the audioTone: %v\n", err)
+		os.Exit(1)
+	}
+
+	sBeepBuffer := beep.NewBuffer(AudioFormat)
+	sBeepBuffer.Append(beep.Take(shortBeepSamples, audioTone))
+
+	lBeepBuffer := beep.NewBuffer(AudioFormat)
+	lBeepBuffer.Append(beep.Take(shortBeepSamples*3, audioTone))
+
+	sDelayBuffer := beep.NewBuffer(AudioFormat)
+	sDelayBuffer.Append(generators.Silence(shortBeepSamples))
+
+	SoundAssets = map[soundType]*beep.Buffer{
+		ShortBeep:  sBeepBuffer,
+		LongBeep:   lBeepBuffer,
+		ShortDelay: sDelayBuffer,
+	}
+}
+
+func MorseCharSound(str string, speed float64) beep.Streamer {
 	buffer := beep.NewBuffer(AudioFormat)
+	resampledSounds := map[soundType]*beep.Buffer{}
+
+	if speed == 1 {
+		resampledSounds = SoundAssets
+	} else {
+		for _type, oldBuffer := range SoundAssets {
+			streamer := oldBuffer.Streamer(0, oldBuffer.Len())
+			resampler := beep.ResampleRatio(4, speed, streamer)
+
+			newBuffer := beep.NewBuffer(AudioFormat)
+			newBuffer.Append(resampler)
+
+			resampledSounds[_type] = newBuffer
+		}
+	}
 
 	for _, r := range str {
 		loopCount := 1
@@ -70,16 +86,22 @@ func MorseCharSound(str string) beep.Streamer {
 		var sound *beep.Buffer
 		switch r {
 		case '.':
-			sound = SoundAssets[ShortBeep]
+			sound = resampledSounds[ShortBeep]
 		case ',':
-			sound = SoundAssets[LongBeep]
+			sound = resampledSounds[LongBeep]
 		default:
-			sound = SoundAssets[ShortDelay]
-			loopCount = 3
+			continue
 		}
 
 		for range loopCount {
 			soundStreamer := sound.Streamer(0, sound.Len())
+			buffer.Append(soundStreamer)
+		}
+
+		if r == '.' || r == ',' {
+			delaySound := resampledSounds[ShortDelay]
+
+			soundStreamer := delaySound.Streamer(0, delaySound.Len())
 			buffer.Append(soundStreamer)
 		}
 	}
