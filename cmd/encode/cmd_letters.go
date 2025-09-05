dad008dc4def98989d34726f11a8b7c39b7e9db4
Jabez Villamarzo <math2btrue@gmail.com>
2025-09-05 12:38:57 +0800

    fix: encode letter/s cmd flag args parse error handling

--- cmd/encode/cmd_letters.go
+++ cmd/encode/cmd_letters.go
@@ -2,7 +2,6 @@ package encode
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -23,13 +22,38 @@ var LetterCmd = &cobra.Command{
 	Use:     "letter",
 	Short:   "Train for decoding letters.",
 	Aliases: []string{"letters"},
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		detectedNonAlphabet := false
 		letters, _ := cmd.Flags().GetString("letters")
+
+		letters = strings.ToLower(letters)
+		letters = strings.Map(func(r rune) rune {
+			if r >= 'a' && r <= 'z' {
+				return r
+			}
+
+			detectedNonAlphabet = true
+			return -1
+		}, letters)
+
+		if detectedNonAlphabet {
+			if len(letters) == 0 {
+				return fmt.Errorf("Error: --letters has effectively nothing in it.")
+			} else {
+				cmd.PrintErrln("Warning: Detected non-alphabet characters in --letters, removing...")
+			}
+		}
+
 		if len(letters) == 0 {
 			levelArg, _ := cmd.Flags().GetUint16("level")
+
 			if int(levelArg) > len(newLettersPerLevel) {
-				cmd.Printf("(Warning: Level is at most %v. Will be set to max.)\n", len(newLettersPerLevel))
+				cmd.PrintErrf("Warning: Level is at most %v. Will be set to max.\n", len(newLettersPerLevel))
 				levelArg = uint16(len(newLettersPerLevel))
+			}
+
+			if levelArg == 0 {
+				return fmt.Errorf("Error: --letters is empty.")
 			}
 
 			for i := range levelArg {
@@ -46,9 +70,10 @@ var LetterCmd = &cobra.Command{
 		p := tea.NewProgram(newLetterModel(dedupedLetters, iterations))
 
 		if _, err := p.Run(); err != nil {
-			fmt.Fprintf(os.Stderr, "Error running the program: %v", err)
-			os.Exit(1)
+			return fmt.Errorf("Error running the program: %v", err)
 		}
+
+		return nil
 	},
 }
 
@@ -72,11 +97,11 @@ func dedupLetters(str string) string {
 func init() {
 	LetterCmd.Flags().UintP("iterations", "n", 0, "Training iterations.")
 
-	LetterCmd.Flags().Uint16P("level", "l", 1, fmt.Sprintf(
+	LetterCmd.Flags().Uint16P("level", "l", 0, fmt.Sprintf(
 		"Level to have for training. Each level adds 3-5 new letters to train. Max level: %v",
 		len(newLettersPerLevel),
 	))
-	LetterCmd.Flags().String("letters", "", "Custom letter pool to train. You probably should start by using --level.")
+	LetterCmd.Flags().String("letters", "", "Custom alphabet pool to train. You probably should start by using --level.")
 
 	LetterCmd.MarkFlagsOneRequired("level", "letters")
 }
